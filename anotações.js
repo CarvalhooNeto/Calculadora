c3d18e3b4894c283a00ebb40d179a344b690a67c
CarvalhoNeto95 <50238288+CarvalhoNeto95@users.noreply.github.com>
2021-05-01 17:22:17 -0300

    teste 3

--- anotações.js
+++ anotações.js
@@ -6,4 +6,16 @@
  *  Podemos utilizar variáveis na construção do css, exemplo no arquivo button.css
  *  Podemos pegar 1 fragmento do css-grid, exemplo esse fica no arquivo calculator.css
  *  O overflow no calculator.css não quebra o tamanho definido, ele esconde
+ *  
+ * 
+ *  TEMPLATE STRING
+ *  Utilizado no arquivo button.jsx, com a justificativa de lidar com javasrcript puro, se fosse apenas
+ *  jsx utilizariamos apenas {}
+ * 
+ *  THIS
+ *  Revisar o conteúdo this
+ * 
+ *  JSX
+ *  A variável classes no button.jsx precisa de um espaço após o nome para concatenar o css
+ *  
  */
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,19 @@
 import React from 'react'
 import './Button.css'
 
-export default props =>
-    <button className = 'button'>{props.label}</button>
+export default props => {/* Se a propriedade props.operation estiver definida, vai retornar a classe operation do css, se não vai ficar vazio*/
+        let classes = 'button '
+
+        classes += props.operation ? 'operation' : ''
+        classes += props.double ? 'double' : ''
+        classes += props.triple ? 'triple' : ''
+       return (
+           <button 
+           onClick= {e => props.click(props.label)}
+           className = {classes}>
+           {props.label}
+
+            </button>
+       )
+ 
+        }
